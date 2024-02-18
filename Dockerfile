fe26d51a617f1e86d8a2d8e7d33858d2dcb732ce
c0883900 <c0883900@mylambton.ca>
2024-02-17 19:44:43 -0500

    changes made

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,5 @@ LABEL build_date "2024-02-17"
 WORKDIR /app
 COPY . .
 RUN npm install
-CMD ["node", "src/index.js"]
+CMD ["node", "app.js"]
 EXPOSE 3000
--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 // app.js
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const SERVER_PORT = process.env.PORT || 3000;
+const NAME = process.env.NAME || "Dheepasri";
 
 // Serve an HTML form
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get('/products', (req, res) => {
-      res.sendFile(__dirname + '/views/products.html');
-    });
+app.get("/products", (req, res) => {
+  res.sendFile(__dirname + "/views/products.html");
+});
 
 app.listen(SERVER_PORT, () => {
-    console.log(`Server listening on port ${SERVER_PORT}`);
+  console.log(`Server listening on port ${SERVER_PORT}`);
+  console.log(`Name : ${NAME}`);
 });
